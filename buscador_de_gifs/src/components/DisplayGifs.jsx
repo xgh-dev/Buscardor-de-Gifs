ab5a1c43202be813793b62dc54e538610da984f5
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2024-12-26 23:49:28 -0600

    ultimos ajustes

--- buscador_de_gifs/src/components/DisplayGifs.jsx
+++ buscador_de_gifs/src/components/DisplayGifs.jsx
@@ -1,25 +1,28 @@
 import useApi from "../hooks/useApi";
 import ImageItem from "./ImageItem";
 
-const DisplayGifs = ({category}) => {
-    const apiKey = 'VDrT08ZDURrHK9LuQk59v4j00tVgJf5c'
-    //definir el endPoint de la api
-    const urlApi = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
+const DisplayGifs = ({ category }) => {
+  const apiKey = "VDrT08ZDURrHK9LuQk59v4j00tVgJf5c";
+  //definir el endPoint de la api
+  const urlApi = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
 
-    const {loading,data} = useApi(urlApi)
+  const { loading, data } = useApi(urlApi);
 
-
-    return (
-        <>
-        <div className="container-gifs">
-            {
-                loading ? data.map(img => (
-                    <ImageItem key={img.id} title={img.title} url={img.images.downsized_medium.url}/>
-                )) : ''
-            }
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container-gifs">
+        {loading
+          ? data.map((img) => (
+              <ImageItem
+                key={img.id}
+                title={img.title}
+                url={img.images.downsized_medium.url}
+              />
+            ))
+          : ""}
+      </div>
+    </>
+  );
+};
 
 export default DisplayGifs;
